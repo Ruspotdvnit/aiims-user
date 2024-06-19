42b8580c884f55319c6417505fef110af8cbba80
Ruspotdvnit <rushikeshrpotdar@gmail.com>
2024-06-19 17:53:06 +0530

    Update FormsController.php
    update jun 19, 17.52

--- user-app/app/Http/Controllers/FormsController.php
+++ user-app/app/Http/Controllers/FormsController.php
@@ -19,13 +19,14 @@ class FormsController extends Controller
         return view('forms');
     }
 
-    public function liveRecpDemo(){
+    public function liveRecpDemo()
+    {
         return view('forms/live-recp-demo');
     }
 
     public function liveDonorRoutine()
     {
-        return view('forms/live-donor'); 
+        return view('forms/live-donor');
     }
 
     public function liveDonorDemo()
@@ -33,93 +34,100 @@ class FormsController extends Controller
         return view('forms/live-donor-demographic');
     }
 
-    public function liveReceive(LiveRoutine $request){
+    public function liveReceive(LiveRoutine $request)
+    {
         $attributes = $request->all();
 
         // dump($attributes);
         $cr_no = $request->get('routine_hmis_cr_no');
 
-        $file1 = 'cdc_'.$cr_no.'.'.$request->routine_cdc_upload_report->extension();
-        $request->routine_cdc_upload_report->storeAs('public/uploads', $file1);
+        if ($request->has('routine_cdc_upload_report')) {
+            $file1 = 'cdc_' . $cr_no . '.' . $request->routine_cdc_upload_report->extension();
+            $request->routine_cdc_upload_report->storeAs('public/uploads', $file1);
+        }
 
-        $file1 = 'hlaMatch_'.$cr_no.'.'.$request->routine_hla_match_upload_report->extension();
-        $request->routine_hla_match_upload_report->storeAs('public/uploads', $file1);
+        if ($request->has('routine_hla_match_upload_report')) {
+            $file1 = 'hlaMatch_' . $cr_no . '.' . $request->routine_hla_match_upload_report->extension();
+            $request->routine_hla_match_upload_report->storeAs('public/uploads', $file1);
+        }
 
-        $file1 = 't_b_'.$cr_no.'.'.$request->routine_t_b_upload_report->extension();
-        $request->routine_t_b_upload_report->storeAs('public/uploads', $file1);
+        if ($request->has('routine_t_b_upload_report')) {
+            $file1 = 't_b_' . $cr_no . '.' . $request->routine_t_b_upload_report->extension();
+            $request->routine_t_b_upload_report->storeAs('public/uploads', $file1);
+        }
 
         $routineData = new Routine;
 
         $columnsToExclude = ['_token', 'proengsoft_jsvalidation', 'routine_cdc_upload_report', 'routine_hla_match_upload_report', 'routine_t_b_upload_report'];
-        
+
         $data = collect($attributes)->except($columnsToExclude)->all();
-        
+
         $routineData->fill($data);
 
         $routineData->save();
 
-        return view('forms/live-donor')->with('message', "Data saved successfully"); 
+        return view('forms/live-donor')->with('message', "Data saved successfully");
     }
 
-    public function liveRecpDemoReceive(LiveRecpReg $request){
+    public function liveRecpDemoReceive(LiveRecpReg $request)
+    {
         $attributes = $request->all();
         // dump($attributes);
-        
+
         $cr_no = $request->get('recp_crn_no');
 
-        if($request->has('recp_family_tree')){
-            $file1 = 'familyTree_'.$cr_no.'.'.$request->recp_family_tree->extension();
+        if ($request->has('recp_family_tree')) {
+            $file1 = 'familyTree_' . $cr_no . '.' . $request->recp_family_tree->extension();
             $request->recp_family_tree->storeAs('public/uploads', $file1);
         }
 
-        if($request->has('recp_if_taking_any_medication__upload_prescription')){
-            $file1 = 'medPrescr_'.$cr_no.'.'.$request->recp_if_taking_any_medication__upload_prescription->extension();
+        if ($request->has('recp_if_taking_any_medication__upload_prescription')) {
+            $file1 = 'medPrescr_' . $cr_no . '.' . $request->recp_if_taking_any_medication__upload_prescription->extension();
             $request->recp_if_taking_any_medication__upload_prescription->storeAs('public/uploads', $file1);
         }
 
         $recpReg = new RecpReg;
 
         $columnsToExclude = ['_token', 'proengsoft_jsvalidation', 'recp_family_tree', 'recp_if_taking_any_medication__upload_prescription'];
-        
+
         $data = collect($attributes)->except($columnsToExclude)->all();
-        
+
         $recpReg->fill($data);
 
         $recpReg->save();
 
-        return view('forms/live-recp-demo')->with('message', "Data saved successfully"); 
+        return view('forms/live-recp-demo')->with('message', "Data saved successfully");
     }
 
-    public function liveDonorDemoReceive(LiveDonorReg $request){
-        
+    public function liveDonorDemoReceive(LiveDonorReg $request)
+    {
+
         $attributes = $request->all();
 
         $cr_no = $request->get('donor_crn_no');
-        
-        if($request->has('donor_family_tree')){
-            $file1 = 'familyTree_'.$cr_no.'.'.$request->donor_family_tree->extension();
+
+        if ($request->has('donor_family_tree')) {
+            $file1 = 'familyTree_' . $cr_no . '.' . $request->donor_family_tree->extension();
             $request->donor_family_tree->storeAs('public/uploads', $file1);
         }
 
-        if($request->has('donor_if_taking_any_medication__upload_prescription')){
-            $file1 = 'medPrescr_'.$cr_no.'.'.$request->donor_if_taking_any_medication__upload_prescription->extension();
+        if ($request->has('donor_if_taking_any_medication__upload_prescription')) {
+            $file1 = 'medPrescr_' . $cr_no . '.' . $request->donor_if_taking_any_medication__upload_prescription->extension();
             $request->donor_if_taking_any_medication__upload_prescription->storeAs('public/uploads', $file1);
         }
 
         $donorReg = new DonorReg;
 
         $columnsToExclude = ['_token', 'proengsoft_jsvalidation', 'donor_family_tree', 'donor_if_taking_any_medication__upload_prescription'];
-        
+
         $data = collect($attributes)->except($columnsToExclude)->all();
-        
+
         $donorReg->fill($data);
 
         $donorReg->save();
 
-        return view('forms/live-donor-demographic')->with('message', "Data saved successfully"); 
+        return view('forms/live-donor-demographic')->with('message', "Data saved successfully");
     }
-
-    
 }
 
 /* 
